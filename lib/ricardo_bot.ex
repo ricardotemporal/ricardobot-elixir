0d6cb5dc45f7001cdc15a363424ae137332fe738
Ricardo <ricardotemporal10@gmail.com>
2025-04-27 16:47:27 -0300

    implement `dog` api

--- lib/ricardo_bot.ex
+++ lib/ricardo_bot.ex
@@ -2,11 +2,15 @@ defmodule RicardoBot do
   use Nostrum.Consumer
   alias Nostrum.Api.Message
   alias RicardoBot.Command.Gato
+  alias RicardoBot.Command.Cachorro
 
   def handle_event({:MESSAGE_CREATE, msg, _ws_status}) do
     cond do
       String.starts_with?(msg.content, "!gato") ->
         Message.create(msg.channel_id, Gato.handle_gato(msg.content))
+
+      String.starts_with?(msg.content, "!cachorro") ->
+        Message.create(msg.channel_id, Cachorro.handle_cachorro(msg.content))
 
       true ->
         :ignore
